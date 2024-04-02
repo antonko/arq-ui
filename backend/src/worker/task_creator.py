0490315aa3cc68e03db67c8948b7bf71ed51211b
Anton Kovalev <antonko@gmail.com>
2024-04-03 00:20:04 +0300

    test: Demo-jobs generator

--- backend/src/worker/task_creator.py
+++ backend/src/worker/task_creator.py
@@ -1,36 +1,86 @@
 import asyncio
 import logging
 import random
-from datetime import datetime
+import sys
 
-from arq import create_pool
-from arq.connections import RedisSettings
+sys.path.insert(0, "/app/src")
+from arq import ArqRedis, create_pool
+from core.config import Settings, get_app_settings
+from core.depends import get_redis_settings
 
 logger = logging.getLogger(__name__)
+settings: Settings = get_app_settings()
 
 
-def get_redis_settings() -> RedisSettings:
-    return RedisSettings(host="redis")
+async def enqueue_job(redis_pool: ArqRedis, is_successful: bool):
+    await redis_pool.enqueue_job(
+        random.choice(
+            [
+                "check_fuel_system",
+                "diagnose_navigation_system",
+                "test_life_support_system",
+                "check_communication_system",
+                "analyze_launch_readiness",
+            ],
+        ),
+        is_successful=is_successful,
+    )
 
-
-async def create_tasks() -> None:
-    print("Start creating tasks")
-
-    redis = await create_pool(get_redis_settings())
-    while True:
-        num_tasks = random.randint(1, 5)
-        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Creating {num_tasks} tasks")
+async def pattern_hill(redis_pool: ArqRedis, max_tasks_per_minute: int):
+    total_minutes = 10
+    for minute in range(1, total_minutes + 1):
+        num_tasks = (
+            round(max_tasks_per_minute * (minute / 5))
+            if minute <= 5
+            else round(max_tasks_per_minute * (2 - minute / 5))
+        )
         for _ in range(num_tasks):
-            await redis.enqueue_job(
-                "process_task",
-                func="Dummy Function",
-                success=random.choice([True, False]),
-            )
-        await asyncio.sleep(60)
+            await enqueue_job(redis_pool, True)
+        
+        if minute < total_minutes:
+            await asyncio.sleep(60)
+
+async def pattern_increasing_errors(redis_pool: ArqRedis, max_tasks_per_minute: int):
+    total_minutes = 5
+    for minute in range(1, total_minutes + 1):
+        num_tasks = random.randint(1, max_tasks_per_minute)
+        error_ratio = minute / 5
+        for _ in range(num_tasks):
+            is_successful = random.random() > error_ratio
+            await enqueue_job(redis_pool, is_successful)
+        if minute < total_minutes:
+            await asyncio.sleep(60)
+
+async def pattern_peak(redis_pool: ArqRedis, max_tasks_per_minute: int):
+    total_minutes = 5
+    for minute in range(1, total_minutes + 1):
+        num_tasks = round(max_tasks_per_minute * (2 * abs(3 - minute) / 5))
+        for _ in range(num_tasks):
+            await enqueue_job(redis_pool, True)
+        
+        if minute < total_minutes:
+            await asyncio.sleep(60)
+
+
+async def pattern_random_distribution(redis_pool: ArqRedis, max_tasks_per_minute: int):
+    total_minutes = 10    
+    for _ in range(total_minutes):
+        num_tasks = random.randint(0, max_tasks_per_minute)
+        for _ in range(num_tasks):
+            await enqueue_job(redis_pool, True)
+        
+        if _ < total_minutes - 1:
+            await asyncio.sleep(60)
+
+
+async def main():
+    redis_pool = await create_pool(get_redis_settings(), default_queue_name=settings.queue_name)
+
+    await pattern_hill(redis_pool, 20)
+    await pattern_increasing_errors(redis_pool, 20)
+    await pattern_random_distribution(redis_pool, 20)
+    await pattern_peak(redis_pool, 20)
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(create_tasks())
-    except Exception as e:
-        print(f"Error occurred: {e}")
+    asyncio.run(main())
--- backend/src/worker/worker.py
+++ backend/src/worker/worker.py
@@ -1,37 +1,111 @@
 import asyncio
+import logging
 import random
+import sys
+
+sys.path.insert(0, '/app/src')
+from core.depends import get_redis_settings
+from models import CommunicationSystemCheck, FuelSystemStatus, LaunchReadinessAnalysis, LifeSupportSystemTestResult, NavigationSystemDiagnostic
 from typing import Any
 
-from arq import Retry, run_worker
-from arq.connections import RedisSettings
-from pydantic import BaseModel
+from arq import run_worker
+from core.config import Settings, get_app_settings
+from functools import wraps
+import asyncio
 
+logger = logging.getLogger(__name__)
+settings: Settings = get_app_settings()
 
-def get_redis_settings() -> RedisSettings:
-    return RedisSettings(host="redis")
+def sleep_decorator(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        await asyncio.sleep(random.uniform(0.1, 20))
+        return await func(*args, **kwargs)
+    return wrapper
 
+@sleep_decorator
+async def check_fuel_system(ctx: dict[str, Any], is_successful: bool) -> str:
+    """Check fuel system status."""
+    if is_successful:
+        return FuelSystemStatus(
+            fuel_level=random.uniform(0, 100),
+            pumps_status=random.choice(["active", "inactive"]),
+        ).model_dump_json()
+    raise Exception("Fuel system check failed")
 
-class TaskResult(BaseModel):
-    status: str
-    message: str = ""
+@sleep_decorator
+async def diagnose_navigation_system(ctx: dict[str, Any], is_successful: bool) -> str:
+    """Diagnose navigation system."""
+    if is_successful:
+        return NavigationSystemDiagnostic(
+            system_status=random.choice(["ok", "error"]),
+            error_code=random.randint(0, 100) if random.random() > 0.5 else None,
+        ).model_dump_json()
+    raise Exception("Navigation system diagnosis failed")
 
+@sleep_decorator
+async def test_life_support_system(ctx: dict[str, Any], is_successful: bool) -> str:
+    """Test life support system."""
+    if is_successful:
+        return LifeSupportSystemTestResult(
+            air_quality=random.choice(["good", "bad"]),
+            water_supply_status=random.choice(["ok", "error"]),
+        ).model_dump_json()
+    raise Exception("Life support system test failed")
 
-async def process_task(ctx: dict[str, Any], func: str, success: bool) -> str:
-    print(f"Processing task with function: {func} and expected success: {success}")
-    await asyncio.sleep(random.randint(1, 15))
-    if success:
-        print("Task processed successfully")
-        return TaskResult(status="ok", message="Task processed successfully").json()
-    else:
-        print("Task processing failed")
-        # raise Exception("Task processing failed")
-        raise Retry(10)
+@sleep_decorator
+async def check_communication_system(ctx: dict[str, Any], is_successful: bool) -> str:
+    """Check communication system."""
+    if is_successful:
+        return CommunicationSystemCheck(
+            signal_strength=random.uniform(0, 100),
+            connection_status=random.choice(["ok", "error"]),
+            satellite_link_quality=random.uniform(0, 100),
+            bandwidth=random.uniform(0, 100),
+            latency=random.uniform(0, 100),
+            error_rate=random.uniform(0, 100),
+            frequency_stability=random.choice(["ok", "error"]),
+            encryption_status=random.choice(["ok", "error"]),
+            backup_system_status=random.choice(["active", "inactive"]),
+            hardware_integrity=random.choice(["ok", "error"]),
+            software_version="1.0",
+            last_maintenance_date="2026-01-01",
+        ).model_dump_json()
+    raise Exception("Communication system check failed")
 
-
+@sleep_decorator
+async def analyze_launch_readiness(ctx: dict[str, Any], is_successful: bool) -> str:
+    """Analyze launch readiness."""
+    if is_successful:
+        return LaunchReadinessAnalysis(
+            readiness_status=random.choice(["ready", "not ready"]),
+            fuel_system_ready=random.choice([True, False]),
+            navigation_system_ready=random.choice([True, False]),
+            life_support_system_ready=random.choice([True, False]),
+            communication_system_ready=random.choice([True, False]),
+            propulsion_system_status=random.choice(["ok", "error"]),
+            structural_integrity=random.choice(["ok", "error"]),
+            onboard_computer_status=random.choice(["ok", "error"]),
+            launch_pad_systems_ready=random.choice([True, False]),
+            weather_conditions=random.choice(["good", "bad"]),
+            crew_readiness=random.choice([True, False]),
+            issues=["Issue 1", "Issue 2"] if random.random() > 0.5 else [],
+            final_inspection_completed=random.choice([True, False]),
+        ).model_dump_json()
+    raise Exception("Launch readiness analysis failed")
+    
 class WorkerSettings:
     redis_settings = get_redis_settings()
-    functions = [process_task]
+    functions = [
+        check_fuel_system,
+        diagnose_navigation_system,
+        test_life_support_system,
+        check_communication_system,
+        analyze_launch_readiness
+        ]
     max_tries = 1
+    queue_name = settings.queue_name
+    allow_abort_jobs = True
 
 
 if __name__ == "__main__":
